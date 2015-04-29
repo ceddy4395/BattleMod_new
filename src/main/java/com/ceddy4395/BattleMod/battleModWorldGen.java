0f1b07055b132e3772596d4b9dba0700adc4a618
ceddy4395 <cedric.willekens@hotmail.com>
2015-04-29 15:33:00 +0200

    ore gen fixed

--- src/main/java/com/ceddy4395/BattleMod/battleModWorldGen.java
+++ src/main/java/com/ceddy4395/BattleMod/battleModWorldGen.java
@@ -4,7 +4,9 @@ package com.ceddy4395.BattleMod;
 import com.ceddy4395.BattleMod.init.BattleModBlocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
@@ -14,6 +16,7 @@ import java.util.Random;
 
 public class battleModWorldGen implements IWorldGenerator {
 
+    @Override
     public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
         switch (world.provider.getDimensionId()) {
             case -1:
@@ -39,7 +42,7 @@ public class battleModWorldGen implements IWorldGenerator {
 
     private void generateOverworld(World world, Random random, int x, int z) {
         generateOre(BattleModBlocks.bronze_Ore, world, random, x, z, 2, 10, 5, 0, 70, Blocks.stone);
-
+        generateOre(BattleModBlocks.copper_Ore, world, random, x, z, 2, 10, 5, 0, 70, Blocks.stone);
 
     }
 
@@ -47,13 +50,17 @@ public class battleModWorldGen implements IWorldGenerator {
     public void generateOre(Block block, World world, Random random, int chunkX, int chunkZ, int minVeinSize, int maxVeinSize, int chance, int minY, int maxY, Block generateIn) {
         int vienSize = minVeinSize + random.nextInt(maxVeinSize - minVeinSize);
         int heightRange = maxY - minY;
-        WorldGenMinable gen = new WorldGenMinable(block, vienSize, generateIn);
+        WorldGenMinable gen = new WorldGenMinable(block.getDefaultState(), vienSize);
         for (int i = 0; i < chance; i++) {
             int xRand = chunkX * 16 + random.nextInt(16);
             int yRand = random.nextInt(heightRange) + minY;
             int zRand = chunkZ * 16 + random.nextInt(16);
-            gen.generate(world, random, xRand, yRand, zRand);
+            gen.generate(world, random, new BlockPos(xRand, yRand, zRand));
 
         }
     }
+
+
+
 }
+
